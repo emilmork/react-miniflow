5976c4340cf4bbe1e660496190c199b5ca02b8b3
Emil Mork <emil.a.mork@gmail.com>
2015-10-01 15:26:58 +0200

    Divide into different files

--- index.js
+++ index.js
@@ -3,37 +3,7 @@ var events = require('events');
 var utils = require('util');
 var React = require('react');
 var { Component } = React;
-
-var Store = function() {
-    this.structure = Immutable.Map();
-
-    if (Store.instance) return Store.instance;
-    Store.instance = this;
-
-    this.init = (structure) => this.structure = Immutable.Map(structure);
-
-    this.set = (type, data) => {
-        this.structure = this.structure.set(type, data);
-        this.emit(type, { id: type, value: this.structure.get(type) });
-    }
-
-    this.update = (type, id, data, append) => {
-        if(!append) {
-            this.structure = this.structure.set(type, data);
-        } else {
-            this.structure = this.structure.set(type, this.structure.get(type).concat(data));
-        }
-        this.emit(type, { id: id, value: this.structure.get(type) });
-    }
-
-    this.get = (type) => this.structure.get(type);
-
-    this.fire = (type, data) => { 
-        this.emit(type, data)
-    }
-}
-
-utils.inherits(Store, events.EventEmitter);
+var Store = require('./Store');
 
 var EnhancedComponent = (Component, listeners) => {
   class View extends Component {
@@ -63,5 +33,5 @@ var EnhancedComponent = (Component, listeners) => {
 }
 
 
-module.exports.Store = new Store();
+module.exports.Store = Store;
 module.exports.EnhancedComponent = EnhancedComponent;
